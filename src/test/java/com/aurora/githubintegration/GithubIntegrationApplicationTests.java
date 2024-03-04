174d0aeaf273b69c7a0fb841aadd820236eabab2
Aurora Kovacic <rory.kovacic@gmail.com>
2024-03-04 14:01:32 -0500

    Initial code

---
+++ src/test/java/com/aurora/githubintegration/GithubIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora.githubintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
